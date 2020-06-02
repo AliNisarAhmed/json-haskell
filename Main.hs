3924be27578ad0b1fe53399f509b3360fee18050
Ali Ahmed <ali.ahmed@punchcard.io>
2020-06-01 23:02:29 -0600

    final json parser

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Applicative (Alternative(..))
-import Data.Char (isDigit)
+import Data.Char (isDigit, isSpace)
 
 main :: IO ()
 main = undefined
@@ -10,6 +10,7 @@ data JsonValue
   = JsonNull
   | JsonBool Bool
   | JsonNumber Integer
+  | JsonString String
   | JsonArray [JsonValue]
   | JsonObject [(String, JsonValue)]
   deriving (Eq, Show)
@@ -20,7 +21,7 @@ newtype Parser a = Parser
 instance Functor Parser where
   fmap f (Parser a) = Parser (fmap (fmap f) . a)
   -- fmap f (Parser p) =
-  --   Parser $ \input -> do 
+  --   Parser $ \input -> do
   --     (input', x) <- p input
   --     Just (input', f x)
 
@@ -41,7 +42,7 @@ instance Alternative Parser where
 
 -------------------------------------------
 
--- Parsers -- 
+-- Parsers --
 
 charP :: Char -> Parser Char
 charP c = Parser f
@@ -70,12 +71,71 @@ spanP f =
     let (token, rest) = span f s
     in Just (rest, token)
 
-jsonNumber :: Parser JsonValue
-jsonNumber = (JsonNumber . readNumber) <$> spanP isDigit
+-- makes sure that the input to a parser is not an empty string
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) =
+  Parser $ \s -> do
+    (s2, xs) <- p s
+    if null xs
+      then Nothing
+      else Just (s2, xs)
 
-readNumber :: String -> Integer
-readNumber = read
+jsonNumber :: Parser JsonValue
+jsonNumber = (JsonNumber . read) <$> notNull (spanP isDigit)
+
+
+-- JSON Strings start and end with a quote
+-- *> :: f a -> f b -> f b
+-- <* :: f a -> f b <- f a
+-- these "apply" the Applicatives, but return only the indicated result
+jsonString :: Parser JsonValue
+jsonString = JsonString <$> stringLiteral
+
+stringLiteral :: Parser String
+stringLiteral = charP '"' *> spanP (/= '"') <* charP '"'
+
+jsonArray :: Parser JsonValue
+jsonArray = JsonArray <$> (charP '[' *> ws *>
+                           elements
+                           <* ws <* charP ']')
+  where
+    elements = sepBy (ws *> charP ',' <* ws) jsonValue
+
+
+ws :: Parser String
+ws = spanP isSpace
+
+
+-- many :: Alternative f => f a -> f [a]
+-- what we are doing here is taking a parser of single b
+  -- and turning it into a parser of list of b
+  -- using the many function from Alternative Typeclass
+  -- more correctly, turning a parser of element-sep-element
+  -- into a parser of element-sep-element-sep-element...
+-- example
+-- runParser (charP 'b') "bbb"
+  -- >>> Just ("bb", 'b')
+-- runParser (many $ charP 'b') "bbb"
+  -- >>> Just ("", "bbb")
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy sep element =
+  (:) <$> element <*> many (sep *> element) <|> pure []
+
+
+jsonObject :: Parser JsonValue
+jsonObject =
+  JsonObject <$>
+    ( charP '{' *> ws *>
+      sepBy (ws *> charP ',' <* ws) pair
+      <* ws <* charP '}'
+    )
+    where
+      pair =
+        (\key _ value -> (key, value)) <$> stringLiteral
+                                       <*> (ws *> charP ':' <* ws)
+                                       <*> jsonValue
 
 -- The final whole JSON value parser
 jsonValue :: Parser JsonValue
-jsonValue = jsonBool <|> jsonNull
+jsonValue =
+  jsonBool <|> jsonNull <|> jsonNumber <|> jsonString <|> jsonArray <|> jsonObject
